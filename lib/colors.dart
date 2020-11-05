05905ba343abe680f867fa98adb074afae9e7d88
victor <victorhmacfer@gmail.com>
2020-11-05 19:12:02 -0300

    initial sketch of bottom part of chat screen

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const lightTeal = Color.fromRGBO(0, 136, 122, 1);
   static const teal = Color.fromRGBO(7, 94, 85, 1);
   static const darkTeal = Color.fromRGBO(5, 76, 68, 1);
   static const white = Color.fromRGBO(255, 255, 255, 1);
   static const white50pct = Color.fromRGBO(255, 255, 255, 0.5);
   static const brightGreen = Color.fromRGBO(0, 204, 63, 1);
+  static const lightBrown = Color.fromRGBO(239, 230, 222, 1);
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -17,6 +17,71 @@ class ChatScreen extends StatelessWidget {
           IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
         ],
       ),
+      body: Container(
+        color: AppColors.lightBrown,
+        constraints: BoxConstraints.expand(),
+      ),
+      bottomSheet: _MyBottomSheet(),
+    );
+  }
+}
+
+class _MyBottomSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.lightBrown,
+      constraints: BoxConstraints(minHeight: 56, maxHeight: 144),
+      padding: EdgeInsets.fromLTRB(6,4,4,4),
+      child: IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center, // TODO: this should be center for one line textfield, bottomCenter for multiple lines
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      // color: Colors.red,
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.sentiment_very_satisfied, size: 26),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        showCursor: true,
+                        maxLines: null,
+                        decoration: InputDecoration.collapsed(
+                            hintText: 'Type a message'),
+                      ),
+                    ),
+                    Container(
+                      // color: Colors.red,
+                      padding: EdgeInsets.all(8),
+                      child: Icon(Icons.camera_alt, size: 25),
+                    ),
+                    SizedBox(width: 4),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(width: 4),
+            Container(
+              height: 48,
+              width: 48,
+              decoration: BoxDecoration(
+                color: AppColors.lightTeal,
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Icon(Icons.mic, color: AppColors.white, size: 26),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -54,7 +119,9 @@ class _AvatarBackButton extends StatelessWidget {
       color: AppColors.teal,
       child: InkWell(
         borderRadius: BorderRadius.circular(24),
-        onTap: () {},
+        onTap: () {
+          Navigator.pop(context);
+        },
         child: Container(
           padding: EdgeInsets.fromLTRB(0, 4, 4, 4),
           child: Row(
