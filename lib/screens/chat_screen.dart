049a9a5768e6c0a078225f00d181f6d2659767b6
victor <victorhmacfer@gmail.com>
2020-11-10 11:33:15 -0300

    sketch of list of messages in chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:wppcc/colors.dart';
 
@@ -20,8 +22,62 @@ class ChatScreen extends StatelessWidget {
       body: Container(
         color: AppColors.lightBrown,
         constraints: BoxConstraints.expand(),
+        child: ListView(
+          children: [
+            MessageTile(Colors.yellow[200], false),
+            MessageTile(Colors.blue[200], false),
+            MessageTile(Colors.orange[200]),
+            MessageTile(Colors.indigo[200], false),
+            MessageTile(Colors.red[200]),
+            MessageTile(Colors.brown[200]),
+            MessageTile(Colors.pink[200], false),
+            MessageTile(Colors.yellow[200], false),
+            MessageTile(Colors.blue[200]),
+            MessageTile(Colors.orange[200]),
+            MessageTile(Colors.indigo[200], false),
+            MessageTile(Colors.red[200], false),
+            MessageTile(Colors.brown[200]),
+            MessageTile(Colors.pink[200], false),
+            MessageTile(Colors.yellow[200]),
+            MessageTile(Colors.blue[200], false),
+            MessageTile(Colors.orange[200]),
+            MessageTile(Colors.indigo[200], false),
+            MessageTile(Colors.red[200]),
+            MessageTile(Colors.brown[200]),
+            MessageTile(Colors.pink[200], false),
+          ],
+        ),
       ),
       bottomSheet: _MyBottomSheet(),
+    );
+  }
+}
+
+class MessageTile extends StatelessWidget {
+  final Color color;
+  final bool sent;
+
+  MessageTile(this.color, [this.sent = true]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 2, horizontal: 16),
+      alignment: sent ? Alignment.centerRight : Alignment.centerLeft,
+      color: color,
+      child: MessageBubble(),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      constraints: BoxConstraints(maxWidth: 336),
+      color: Colors.green[100],
+      child: Text(lorem()),
     );
   }
 }
@@ -32,14 +88,15 @@ class _MyBottomSheet extends StatelessWidget {
     return Container(
       color: AppColors.lightBrown,
       constraints: BoxConstraints(minHeight: 56, maxHeight: 144),
-      padding: EdgeInsets.fromLTRB(6,4,4,4),
+      padding: EdgeInsets.fromLTRB(6, 4, 4, 4),
       child: IntrinsicHeight(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Expanded(
               child: Container(
-                alignment: Alignment.center, // TODO: this should be center for one line textfield, bottomCenter for multiple lines
+                alignment: Alignment
+                    .center, // TODO: this should be center for one line textfield, bottomCenter for multiple lines
                 decoration: BoxDecoration(
                   color: AppColors.white,
                   borderRadius: BorderRadius.circular(24),
@@ -141,4 +198,21 @@ class _AvatarBackButton extends StatelessWidget {
       ),
     );
   }
+}
+
+//100 word lorem
+const fullLorem =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor, dui ac pellentesque vestibulum, dolor elit dapibus massa, ac consequat ante nisi vitae urna. Morbi lobortis fringilla lorem, a maximus enim porttitor id. Cras vel massa mauris. Aenean at erat efficitur, semper eros nec, sodales tellus. Integer malesuada vulputate mattis. Sed pretium ultrices magna, ac porta ipsum rhoncus ut. Nulla imperdiet ipsum sit amet odio viverra consectetur. Donec cursus mollis erat, quis efficitur neque euismod a. Vestibulum vel massa non mi maximus pellentesque et a lorem. Curabitur vitae facilisis erat, nec aliquam tortor. Nunc vehicula vitae risus quis vehicula. Praesent.";
+
+// generates random length(1 to 100 words) lorem.
+String lorem() {
+  var words = fullLorem.split(" ");
+
+  var randTen = Random().nextInt(10);
+  var max = (randTen < 8) ? 12 : 50;
+
+  var rand = Random().nextInt(max);
+  
+  var sub = words.sublist(0, rand);
+  return sub.join(" ");
 }
